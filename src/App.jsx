560098e8120d581af2a121e188dc6e9b0ee4a03b
Kuan314 <limzhengkuan@hotmail.com>
2022-11-23 20:47:30 +0800

    edit router

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
 		<Routes>
 			<Route exact path="/" element={<Home />} />
-			<Route path="about" element={<About />} />
+			<Route path="/about" element={<About />} />
 		</Routes>
 	)
 }
