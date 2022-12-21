df883e25c1ce5f336bf0105bcb416a5ec82e5145
Kuan314 <limzhengkuan@hotmail.com>
2022-12-21 23:31:28 +0800

    set Counter ui

--- src/miniChallenges/Counter.js
+++ src/miniChallenges/Counter.js
@@ -2,20 +2,25 @@ import React, { useState } from "react";
 
 function Counter() {
   const [num, getNum] = useState(0);
-  
+
   return ( 
     <>
-    <h1>Counter</h1>
-    <div className="p-2">
-      {num}
-    </div>
-    <div className="flex justify-between items-center">
-      <button>
-        +
-      </button>
-      <button>
-        -
-      </button>
+    <div className="flex flex-col items-center pt-4">
+      <h1 className="text-5xl">Counter</h1>
+      <div className="p-3 my-2 text-3xl rounded-md">
+        {num}
+      </div>
+      <div className="flex">
+        <button className="w-12 p-2 m-2 text-xl border-2 rounded-md active:bg-[#000], ">
+          +
+        </button>
+        <button className="w-12 p-2 m-2 text-xl border-2 rounded-md">
+          -
+        </button>
+        <button className="p-2 m-2 text-xl border-2 rounded-md">
+          reset
+        </button>
+      </div>
     </div>
     </>
   )
