80add9f6df765926857f3f12b23ba359e26a48e5
Kuan314 <limzhengkuan@hotmail.com>
2022-12-21 15:49:52 +0800

    fix url problem

--- src/pages/MiniChallenge.js
+++ src/pages/MiniChallenge.js
@@ -3,7 +3,7 @@ function MiniChallenge() {
 	return (
 		<div className="grid grid-cols-4 gap-6 bg-primary mx-8 mt-4">
 			<div className="bg-secondary m-4">
-				<a href="/miniChallenge/Counter" className="flex justify-center text-xl py-8">
+				<a href=".\#\miniChallenge\Counter" className="flex justify-center text-xl py-8">
 					Counter
 				</a>
 			</div>
