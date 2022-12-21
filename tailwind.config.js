6739c2f4febda84e865310d47bcce03cd665bf2a
Kuan314 <limzhengkuan@hotmail.com>
2022-12-22 00:01:45 +0800

    edit tailwind color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    colors: {
-      'primary': '#FBE0C3',
-      'secondary': '#FFBB98',
-      'textPrimary': '#7D8E95',
-      'textSecondary': '#344648',
+    extend: {
+      colors: {
+        'primary': '#FBE0C3',
+        'secondary': '#FFBB98',
+        'textPrimary': '#7D8E95',
+        'textSecondary': '#344648',
+      }
     },
-    extend: {},
   },
   plugins: [],
 }
