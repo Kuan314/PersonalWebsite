9f9b4e6c365dda311f071a1caf6692f7e100e765
Kuan314 <limzhengkuan@hotmail.com>
2022-12-21 23:31:03 +0800

    setting mini challenge ui

--- src/pages/MiniChallenge.js
+++ src/pages/MiniChallenge.js
@@ -3,17 +3,17 @@ function MiniChallenge() {
 	return (
 		<div className="grid grid-cols-4 gap-6 bg-primary mx-8 mt-4">
 			<div className="bg-secondary m-4">
-				<a href=".\#\miniChallenge\Counter" className="flex justify-center text-xl py-8">
+				<a href="./#/miniChallenge/Counter" className="flex justify-center text-xl py-8">
 					Counter
 				</a>
 			</div>
 			<div className="bg-secondary m-4">
-				<a href=".\Counter" className="flex justify-center text-xl py-8">
+				<a href="./#/miniChallenge/Counter" className="flex justify-center text-xl py-8">
 					Counter
 				</a>
 			</div>
 			<div className="bg-secondary m-4">
-				<a href=".\Counter" className="flex justify-center text-xl py-8">
+				<a href="./#/miniChallenge/Counter" className="flex justify-center text-xl py-8">
 					Counter
 				</a>
 			</div>
