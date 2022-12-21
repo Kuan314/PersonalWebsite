93a7ffb728cde2a36f665aa0947c41c944f32c11
Kuan314 <limzhengkuan@hotmail.com>
2022-12-22 00:22:17 +0800

    edit homepage text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 function Home() {
 		return (
 			<div className="mt-4 h-96 flex justify-center items-center">
-				<h1 className="text-6xl text-textPrimary font-bold hover:text-7xl">
+				<h1 className="text-8xl text-textPrimary font-bold hover:text-9xl">
 					Hi, I'm Kuan
 				</h1>
 
