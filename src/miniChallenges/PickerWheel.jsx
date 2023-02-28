e83ed41ed2060b6033a41cb5aaa4b0883bc4dbbc
Kuan314 <limzhengkuan@hotmail.com>
2023-02-28 22:25:04 +0800

    working on Picker Wheel

--- src/miniChallenges/PickerWheel.jsx
+++ src/miniChallenges/PickerWheel.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-
-const selection = [
-	
-];
+import React, { useState } from "react";
 
 const PickerWheel = () => {
+
+	const selections = [];
+	const [selection, setSelection] = useState("");
+
+	const addSelection = () => {
+    if(selection.match(/^[\S]+$/)){
+      selections.push(selection);
+    } else {
+      alert("Your input is not valid!");
+    }
+
+    setSelection("");
+	}
+
 	return (
 		<>
 		<div className="flex justify-center">
-				<div className="w-60 h-60 rounded-full bg-secondary">
-					{selection}
-				</div>
+			<div className="w-60 h-60 rounded-full bg-secondary">
+				{selection}
+			</div>
+
+      <form onSubmit={addSelection}>
+        <input className="px-1 border rounded-md" type="text" value={selection} onChange={e => setSelection(e.target.value)} />
+        <button className="px-1 mx-2 border-textSecondary rounded-md border-2">Submit</button>
+      </form>
 				
 		</div>
 		</>
