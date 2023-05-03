2c82d2385a99b902caedd548194cbf6cfa1bd434
Kuan314 <limzhengkuan@hotmail.com>
2023-05-03 17:16:11 +0800

    add status to check working status

--- src/pages/Challenges.jsx
+++ src/pages/Challenges.jsx
@@ -4,18 +4,22 @@ const challengesList = [
 	{
 		name: "Counter",
 		href: "./#/Challenges/Counter",
+		status: true,
 	},
 	{
 		name: "Todo",
-		href: "./#/Challenges/Todo"
+		href: "./#/Challenges/Todo",
+		status: true,
 	},
 	{
 		name: "Lucky Wheel",
-		href: "./#/Challenges/Wheel"
+		href: "./#/Challenges/Wheel",
+		status: true,
 	},
 	{
 		name: "Snake",
-		href: "./#/Challenges/Snake"
+		href: "./#/Challenges/Snake",
+		status: false,
 	},
 ];
 
@@ -24,7 +28,7 @@ function Challenges() {
 		<div className="grid grid-cols-4 gap-6 bg-primary mx-8 mt-4">
 			{challengesList.map((challenge) => {
 				return (
-					<div className="bg-secondary m-4">
+					<div className={`bg-secondary m-4 ${challenge.status ? '' : "opacity-60 pointer-events-none"}`}>
 						<a href={challenge.href} className="flex justify-center text-xl py-8">
 							{challenge.name}
 						</a>
