a671b28ea5b710041c1e7689899a9a2be53f5556
Kuan314 <limzhengkuan@hotmail.com>
2023-05-05 11:37:21 +0800

    import lazy & suspense

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import MiniChallenge from './pages/Challenges';
 import Website from './pages/Website';
 import Header from './component/Header';
+import Loading from './component/Loading';
 
 // lazy loading
 const Counter = lazy(() => import('./miniChallenges/Counter'));
@@ -17,23 +18,23 @@ const App = () => {
   return (
 		<div className='bg-primary h-screen'>
 			<Header />
-			<Routes>
-				{/* Main Pages */}
-				<Route exact path="/" element={<Home />} />
-				<Route path="/Challenges" element={<MiniChallenge />} />
-				<Route path="/website" element={<Website />} />
-				<Route path="/about" element={<About />} />
-
-				{/* Mini Challenges */}
-				<Suspense fallback={<div>loading...</div>}>
+			
+			<Suspense fallback={<Loading />}>
+				<Routes>
+					{/* Main Pages */}
+					<Route exact path="/" element={<Home />} />
+					<Route path="/Challenges" element={<MiniChallenge />} />
+					<Route path="/website" element={<Website />} />
+					<Route path="/about" element={<About />} />
+				
+					{/* Mini Challenges */}
 					<Route path="/Challenges/Counter" element={<Counter />} />
 					<Route path="/Challenges/Todo" element={<Todo />} />
 					<Route path="/Challenges/Wheel" element={<Spinner />} />
 					<Route path="/Challenges/Snake" element={<Snake />} />
 					<Route path="/Challenges/CountDown" element={<CountDown />} />
-				</Suspense>
-
-			</Routes>
+				</Routes>
+			</Suspense>
 		</div>
 	)
 }
