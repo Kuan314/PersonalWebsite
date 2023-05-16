ad40b642bb5ead3d0b959e23ca03436c35b2f238
Kuan314 <limzhengkuan@hotmail.com>
2023-05-16 16:08:19 +0800

    add website landing page

---
+++ src/pages/Restaurant/RestaurantIndex.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RestaurantIndex = () => {
+  return (
+    <div className="bg-white">
+      restaurant first page
+    </div>
+  )
+}
+
+export default RestaurantIndex;
