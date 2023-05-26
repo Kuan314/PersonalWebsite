223d474c4f8ff21dbbf9d68002b4abc0eb2ea833
Kuan314 <limzhengkuan@hotmail.com>
2023-05-26 10:54:46 +0800

    implement reserve page

--- src/pages/Restaurant/Restaurant.jsx
+++ src/pages/Restaurant/Restaurant.jsx
@@ -3,8 +3,9 @@ import Header from "./RestaurantComponents/Header";
 import Footer from "./RestaurantComponents/Footer";
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./RestaurantPages/Mainpage";
-import Foods from "./RestaurantPages/Menu";
+import Menu from "./RestaurantPages/Menu";
 import Contact from "./RestaurantPages/Contact";
+import Reserve from "./RestaurantPages/Reserve";
 
 const RestaurantIndex = () => {
   return (
@@ -13,8 +14,9 @@ const RestaurantIndex = () => {
 
     <Routes>
 			<Route index path="/" element={<MainPage />} />
-      <Route path="Foods" element={<Foods />} />
+      <Route path="Menu" element={<Menu />} />
       <Route path="Contact" element={<Contact />} />
+      <Route path="Reserve" element={<Reserve />} />
     </Routes>
 
     <Footer />
--- src/pages/Restaurant/RestaurantComponents/Header.jsx
+++ src/pages/Restaurant/RestaurantComponents/Header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     <div className="py-1 px-2 flex items-center bg-orange-300">
       <img src={Logo} alt="White Sugar Restaurant" className="w-14 h-14 block mx-auto" />
       <div className="flex gap-8 absolute right-4 ">
-        <a href="" className="">Menu</a>
-        <a href="" className="">Contact</a>
-        <a href="" className="">Reserve</a>
+        <a href="Menu" className="">Menu</a>
+        <a href="Contact" className="">Contact</a>
+        <a href="Reserve" className="">Reserve</a>
       </div>
     </div>
   );
