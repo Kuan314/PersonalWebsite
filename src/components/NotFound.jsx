5130f4dedc7585847f622bc5ca2c1bd5aadd79f3
kuan <kuan.ilinke@gmail.com>
2024-02-03 15:03:22 +0800

    change NotFound css

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center font-bold bg-white">
-      <span className="text-8xl">404</span>
-      <span className="text-lg">Not Found</span>
+    <div className="flex flex-col items-center font-bold my-8 ">
+      <span className="text-8xl text-white animate-bounce">404</span>
+      <span className="text-lg text-white">Not Found</span>
     </div>
   );
 };
